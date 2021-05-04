e20b29ff97fc1687549cede42a18a8872929c608
lindievangils <lr.vangils@student.avans.nl>
2021-05-04 09:53:51 +0200

    iets aangepast

--- hellogit/src/com/avans/hellogit/HelloGitHub.java
+++ hellogit/src/com/avans/hellogit/HelloGitHub.java
@@ -2,6 +2,7 @@ package com.avans.hellogit;
 
 public class HelloGitHub {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello Git");
+        System.out.println("Sorry, we zijn nog niet bij GitHub aangekomen.");
     }
 }
